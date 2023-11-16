349924dab5bc0fe9e923ae1df558f9a5ea9d2bcf
ehdrjs13 <ehdrjs070102@gmail.com>
2023-11-17 00:48:51 +0900

    added makeQR

--- makeQR.py
+++ makeQR.py
@@ -1,1 +1,22 @@
+from processData import GetPersonalData
+# from makeQR import GetQR
+import qrcode
+
 class GetQR():
+    def __init__(self) -> None:
+        self.personalData = GetPersonalData('visitorList.xlsx')
+        self.data = self.personalData.GetCode()
+        for i in range(self.data.shape[1]):
+            num_four = str(i+1).zfill(4)
+            self.makeQR(num_four)
+        return
+    
+    def makeQR(self, num):
+        
+        code = self.data[num][3]
+        img = qrcode.make(code)
+        img.save(f"qrcodes/{num}.jpg")
+
+
+a = GetQR()
+
