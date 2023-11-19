ee70b49424ef8a09d8334deb0e2982daeaa0d484
ehdrjs13 <ehdrjs070102@gmail.com>
2023-11-19 18:20:18 +0900

    improved user experience

--- greeting_main.py
+++ greeting_main.py
@@ -2,8 +2,18 @@ import cv2
 from pyzbar.pyzbar import decode
 from retrieveID import RetrieveByNum
 import pandas as pd
+# from tkinter import messagebox
+import winsound
+import time
+
+print('Wait for a Second...\n\n')
 
 SearchData = RetrieveByNum()
+
+print('\n\n[2023 Seoul High School KyungheeJe Visitor Check-In System]\n\n Scan QR to Check-IN..')
+
+
+
 
 def scan_qr_code():
 
@@ -16,6 +26,7 @@ def scan_qr_code():
         decoded_objects = decode(frame)
 
         for obj in decoded_objects:
+
            
             points = obj.polygon
             if len(points) > 4:
@@ -26,6 +37,8 @@ def scan_qr_code():
             qr_data = obj.data.decode("utf-8")
             
             datas = SearchData.searchID(qr_data)
+
+
 
             return [datas['이름'],datas['학교'],datas['개인코드']]
 
@@ -40,16 +53,34 @@ def scan_qr_code():
 
 def getDataFromQR():
 
-    datas = scan_qr_code()
 
+
+    datas = scan_qr_code()
+    winsound.Beep(2000,100)
+    winsound.Beep(1500,180)
+
+    
+    
     name = datas[0]
     school = datas[1]
     code = datas[2]
 
-    print('이름: ',name,'\n\n\n학교: ',school,'\n\n\nGate: ', code[0],'\n\n\n\n',code,'\n\n\n\n')
+    
 
+  
+
+    print(f'이름: {name}\n\n\n학교: {school}\n\n\nGate:  {code[0]}\n\n\n\n{code}\n\n\n\n환영합니다!\n')
+
+
+
+    
 
 while True:
     
-    getDataFromQR()
+    try:
+        getDataFromQR()
+    except:
+        winsound.Beep(2000,2000)
+        print("\033[91mERROR:올바른 형태의 데이터가 아닙니다.\033[0m")
+
 
