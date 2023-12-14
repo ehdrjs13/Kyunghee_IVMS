5f13d3f1f5c1b7e939f838d4cb1a4ccb09aff662
ehdrjs13 <ehdrjs070102@gmail.com>
2023-12-14 22:05:53 +0900

    implement database and api server

--- getSQLdb.py
+++ getSQLdb.py
@@ -2,12 +2,17 @@ from approachData import approachData
 import sqlite3
 import pandas as pd
 
+
 a = approachData()
 
-df = a.sheet
+df = a.sheet.transpose()
+df = df.reset_index(drop=True)
+df.index.name = 'indexing'
+
+print(df)
 
 conn = sqlite3.connect('visitorlist.db')
-df.to_sql('visitors', conn, index=False, if_exists='replace')
+df.to_sql('visitors', conn, index=True, if_exists='replace')
 
 conn.close()
 
--- server.py
+++ server.py
@@ -1,9 +1,78 @@
 from flask import Flask, request, jsonify
 import requests
 import pandas as pd 
+import sqlite3
+import logging
+from datetime import datetime
+
+now = datetime.now()
+
+
 
 app = Flask(__name__)
 
-@app.route('/mainEntrial/<id>', methods = ['POST'])
+
+logging.basicConfig(filename='Server_Log/app.log', level=logging.DEBUG)
+
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+
+@app.route('/mainEntrial/<id>', methods = ['GET'])
 def search(id):
+    con = sqlite3.connect('visitorlist.db')
+    cursor = con.cursor()
     
+    index = int(id[1:5]) - 1
+    cursor.execute(f"SELECT * FROM visitors WHERE indexing = {index}")
+    data = cursor.fetchone() #tuple
+
+    #체크값 1로 바꾸기
+    cursor.execute(f"UPDATE visitors SET 'Check' = 1 WHERE indexing = {index}")
+
+    con.commit()
+
+    con.close()
+
+    app.logger.info(f'\n{now.time()}  |  Entrial : {data[4]} ----------')
+
+
+
+
+
+    return jsonify(data=data)
+
+@app.route('/recovery/<id>', methods = ['GET'])
+def recovery(id):
+    con = sqlite3.connect('visitorlist.db')
+    cursor = con.cursor()
+    
+    index = int(id[1:5]) - 1
+
+    #체크값 1로 바꾸기
+    cursor.execute(f"UPDATE visitors SET 'Check' = 0 WHERE indexing = {index}")
+
+    con.commit()
+
+    con.close()
+
+    app.logger.info(f'\n{now.time()}  |  Recover : {index+1} ----------')
+
+    return 'Done!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+#<json 전송 형식>
+#     {
+#     "data": [
+#         0,
+#         "홍길동",
+#         "상문고",
+#         "0001",
+#         "A0001SM",
+#         0
+#     ]
+# }
